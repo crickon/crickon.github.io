20169108d03abbfb146c17d570171b8cc1485876
notaphoney <crickonalt1@gmail.com>
2018-11-02 21:31:25 -0400

    re

--- assignments/script.js
+++ assignments/script.js
@@ -1,22 +1,31 @@
 function load() {
 fetch('input.txt')
   .then(response => response.text())
-   .then(text => parse(text))
+  .then(text => parseText(text))
+
+fetch('manual.txt')
+	.then(response => response.text())
+	.then(text => parseText(text))
 }
 
-function parse(t) {
-	var arr = t.split("\n");
+function parseText(t) {
+	var arr = t.split("\n"); //split by lines
 	var i = 2;
 	console.log(arr.length);
 	while(i < arr.length){
-		add_Row(arr[i-2], arr[i-1], arr[i]);
+		var today = new Date();
+		var date = new Date(arr[i]);
+		if (date.getMonth() >= today.getMonth() && date.getDate() >= today.getDate()){
+			var days = date.getDate()-today.getDate();
+			var assignment = {class: arr[i-2], description: arr[i-1], date: arr[i], days: days};
+			addAssignment(assignment);
+	}
 		i = i + 4;
-		console.log(i);
 	}
 }
 
-function add_Row(cl, ass, date) {
+function addAssignment(assignment) {
 	var table = document.getElementById("data");
 	var len = (table.rows.length);
-	var row = table.insertRow(len).outerHTML="<tr><td>"+cl+"</td><td>"+ass+"</td><td>"+date+"</td></tr>";
+	var row = table.insertRow(len).outerHTML="<tr><td>"+assignment.class+"</td><td>"+assignment.description+"</td><td>"+assignment.date+"</td><td>"+assignment.days+"</td></tr>";
 }
